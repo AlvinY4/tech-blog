d8f567181ef53bd99c47f34a63585cd851d04463
Alvin Yearby <leroy.yearby@gmail.com>
2022-01-23 22:37:53 -0500

    Added a Procfile.

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,32 +3,32 @@ const { Post } = require('../models');
 const postData = [
   {
     title: 'Mysql',
-    post_text: 'MySQL is free and open-source software under the terms of the GNU General Public License, and is also available under a variety of proprietary licenses.',
+    post_content: 'MySQL is free and open-source software under the terms of the GNU General Public License, and is also available under a variety of proprietary licenses.',
     user_id: 1,
   },
   {
     title: 'Handlebars',
-    post_text: 'A Javascript library used to create reusable webpage templates. The templates are combination of HTML, text, and expressions.',
+    post_content: 'A Javascript library used to create reusable webpage templates. The templates are combination of HTML, text, and expressions.',
     user_id: 2,
   },
   {
     title: 'Handlebars Partials',
-    post_text: 'Partials are normal Handlebars templates that may be called directly by other templates.',
+    post_content: 'Partials are normal Handlebars templates that may be called directly by other templates.',
     user_id: 2,
   },
   {
     title: 'Cookies',
-    post_text: 'Cookies are simple, small files/data that are sent to client with a server request and stored on the client side.',
+    post_content: 'Cookies are simple, small files/data that are sent to client with a server request and stored on the client side.',
     user_id: 3,
   },
   {
     title: 'Dotenv',
-    post_text: 'Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env .',
+    post_content: 'Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env .',
     user_id: 4,
   },
   {
     title: 'Express.js',
-    post_text: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
+    post_content: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
     user_id: 5,
   },
 ]
--- server.js
+++ server.js
@@ -1,46 +1,34 @@
-const path = require('path');
-require('dotenv').config();
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-
 const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
 const helpers = require('./utils/helpers');
-
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
-
+const session = require('express-session');
+const app = express();
+const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
-    secret: 'super secret secret',
-    cookie: {
-        // Session will automatically expire in 10 minutes
-        expires: 10 * 60 * 1000
-  },
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize
     })
-  };
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+};
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-  });
+});
+
 
 
 
